7fe5bd6d3ac41ab398d45e0f16ef44e8a724ae77
Sina Nafei Yazdi <prosny@live.com>
2019-06-25 21:10:31 +0430

    Add OperationResult Class

---
+++ SamDevs.Infrastructures/SamDevs.Infrastructure/Helpers/OperationResult.cs
@@ -0,0 +1,18 @@
+﻿namespace SamDevs.Infrastructure.Helpers
+{
+    public class OperationResult<TData>
+    {
+        public string Code { get; set; }
+        public string Message { get; set; }
+        public bool Success { get; set; }
+        public TData Data { get; set; }
+
+        public OperationResult(string code, bool success, string message = null, TData data = default)
+        {
+            Code = code;
+            Success = success;
+            Message = message;
+            Data = data;
+        }
+    }
+}
